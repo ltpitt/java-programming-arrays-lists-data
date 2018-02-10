312863472e9581070108f606745cad7e4dfa723e
ltpitt <d.nastri@gmail.com>
2018-02-10 13:15:54 +0100

    feat: Added WordPlay class, improved CaesarCipher

--- CaesarCipher/CaesarCipher.java
+++ CaesarCipher/CaesarCipher.java
@@ -8,14 +8,19 @@ import edu.duke.*;
  */
 public class CaesarCipher {
     public String encrypt(String input, int key) {
-        StringBuilder encrypted = new StringBuilder(input.toUpperCase());
+        StringBuilder encrypted = new StringBuilder(input);
         String alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
         String shiftedAlphabet = alphabet.substring(key) + alphabet.substring(0,key);
         for (int i = 0; i < encrypted.length(); i++) {
             char currChar = encrypted.charAt(i);
+            boolean isCurrCharUppercase = Character.isUpperCase(currChar);
+            currChar = Character.toUpperCase(currChar);
             int idx = alphabet.indexOf(currChar);
             if (idx != -1) {
                 char newChar = shiftedAlphabet.charAt(idx);
+                if (!isCurrCharUppercase) {
+                    newChar = Character.toLowerCase(newChar);
+                }
                 encrypted.setCharAt(i, newChar);
             }
         }
@@ -23,6 +28,18 @@ public class CaesarCipher {
     }
     public void testEncrypt() {
         System.out.println(encrypt("Hello there", 3));
+    }
+    public void testCaesar() {
+        int key = 17;
+        FileResource fr = new FileResource();
+        String message = fr.asString();
+        String encrypted = encrypt(message, key);
+        System.out.println("key is " + key + "\n" + encrypted);    
+    }
+    public String encryptTwoKeys(String input, int key, int key2) {
+        return "";
+    }
+    public void testEncryptTwoKeys() {
     }
 }
 
--- CaesarCipher/WordPlay.java
+++ CaesarCipher/WordPlay.java
@@ -7,8 +7,8 @@
  */
 public class WordPlay {
     public boolean isVowel(char ch) {
-        String vowels = "aeiou";
-        if (vowels.toLowerCase().contains(Character.toString(ch)) || vowels.toUpperCase().contains(Character.toString(ch)) ) {
+        String vowels = "aeiouAEIOU";
+        if (vowels.indexOf(ch) != -1 ) {
             return true;
         } else {
             return false;
@@ -20,5 +20,38 @@ public class WordPlay {
         ch = 'a';
         System.out.println("Is a a vowel: " + isVowel(ch));
     }
-
+    public String replaceVowels(String phrase, char ch) {
+        StringBuilder returnPhrase = new StringBuilder(phrase);        
+        for (int i = 0; i < returnPhrase.length(); i++) {
+            char currChar = returnPhrase.charAt(i);
+            if ( isVowel(currChar) ) { 
+                    System.out.println("Found vowel: " + currChar);
+                    returnPhrase.setCharAt(i, '*');
+            }
+        }
+        return returnPhrase.toString();
+    }
+    public void testReplaceVowels() {
+        System.out.println(replaceVowels("Davide", '*'));
+    }
+    public String emphasize(String phrase, char ch) {
+        StringBuilder returnPhrase = new StringBuilder(phrase);
+        for (int i = 0; i < returnPhrase.length(); i++) {
+            char currChar = returnPhrase.charAt(i);
+            if ( Character.toUpperCase(currChar) == ch || Character.toLowerCase(currChar) == ch) {
+                if (i % 2 == 0) {
+                    returnPhrase.setCharAt(i, '*');
+                } else {
+                    returnPhrase.setCharAt(i, '+');
+                }
+            }
+        }
+        return returnPhrase.toString();
+    }
+    public void testEmphasize() {
+        System.out.println("Expected result: dn* ctg+*+ctg+");
+        System.out.println(emphasize("dna ctgaaactga",'a'));
+        System.out.println("Expected result: M+ry Bell+ +br*c*d*br+");        
+        System.out.println(emphasize("Mary Bella Abracadabra",'a'));
+    }
 }
