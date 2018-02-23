c90f606cf040a393364be46e59424a3f6e4302e9
Davide Nastri <d.nastri@gmail.com>
2018-02-23 17:54:30 +0100

    feat: Implemented new methods in LogAnalyzer class

--- WebServerLogsModule/LogAnalyzer.java
+++ WebServerLogsModule/LogAnalyzer.java
@@ -1,53 +1,132 @@
 package WebServerLogsModule;
 
 /**
- * Write a description of class LogAnalyzer here.
+ * A Java class to analyze Apache Webserver logs.
  * 
- * @author (your name) 
- * @version (a version number or a date)
+ * @author Davide Nastri
+ * @version 2/22/2018
  */
-
 import java.util.*;
 import edu.duke.*;
+import java.text.*; 
 
 public class LogAnalyzer
 {
-     private ArrayList<LogEntry> records;
-     
-     public LogAnalyzer() {
-         records = new ArrayList<LogEntry>();
-     }
-        
-     public void readFile(String filename) {
-         FileResource fr = new FileResource();
-         for (String line : fr.lines()) {
-             System.out.println("Line\t" + line);
-             String ip = line.substring(0,line.indexOf(" "));
-             line = line.substring(line.indexOf(ip));
-             String date = line.substring( line.indexOf("[") + 1, line.indexOf("]") );
-             line = line.substring(line.indexOf("\"")+1);
-             String request = line.substring(0 , line.indexOf("\"") );
-             line = line.substring(request.length() + 2);
-             String status = line.substring(0, line.indexOf(" " ));
-             String bytes = line.substring(line.indexOf(" " )+1);
-             System.out.println("Ip\t" + ip);
-             System.out.println("Date\t" + date);
-             System.out.println("Request\t" + request);
-             System.out.println("Status\t" + status);
-             System.out.println("Bytes\t" + bytes);
-             System.out.println();
-             
-             //records.add(new LogEntry("1.2.3.4", new Date(), "example request", 200, 500));
-            }
-     }
-     public void testReadFile() {
-            readFile("test.txt");
+    private ArrayList<LogEntry> records;
+    private static final SimpleDateFormat dateFormat = new SimpleDateFormat("MMM DD", Locale.US);
+    public LogAnalyzer() {
+        records = new ArrayList<LogEntry>();
+    }
+    public void readFile(String filename) {
+    FileResource f = new FileResource(filename);
+    for (String line: f.lines()) {
+        LogEntry log = WebLogParser.parseEntry(line);
+        records.add(log);
+    }
+    }
+    public void printAll() {
+        for (LogEntry le : records) {
+            System.out.println(le);
         }
-     public void printAll() {
-         for (LogEntry le : records) {
-             System.out.println(le);
-         }
-     }
-     
-     
+    }
+    public void printAllHigherThanNum(int num) {
+        for (LogEntry le : records) {
+            int statusCode = le.getStatusCode();
+            if (statusCode > num) {
+                System.out.println(statusCode);
+            }
+        }
+    }
+    public int countUniqueIPs() {
+        ArrayList<String> uniqueIPs = new ArrayList<String>();
+        for (LogEntry le : records) {
+            String ipAddr = le.getIpAddress();
+            if (!uniqueIPs.contains(ipAddr)) {
+                uniqueIPs.add(ipAddr);
+            }
+        }
+        return uniqueIPs.size();
+    }
+    public ArrayList<String> uniqueIPVisitsOnDay(String someday) {
+        ArrayList<String> uniqueIPVisits = new ArrayList<String>();
+        for (LogEntry le : records) {
+            String ipAddress = le.getIpAddress();
+            String accessTime = le.getAccessTime().toString();
+            accessTime = accessTime.substring(4, 10);
+            //System.out.println("Specified date is " + someday);
+            //System.out.println("Date on log is " + accessTime);
+            if (someday.equals(accessTime)){
+                if (!uniqueIPVisits.contains(ipAddress)) {
+                    uniqueIPVisits.add(ipAddress);
+                }
+            }
+        }
+        return uniqueIPVisits;
+    }
+    public int countUniqueIPsInRange(int low, int high) {
+        ArrayList<String> uniqueIPsInRange = new ArrayList<String>();
+        for (LogEntry le: records) {
+            String ipAddress = le.getIpAddress();
+            if(le.getStatusCode() >= low && le.getStatusCode()<= high) {
+                if (!uniqueIPsInRange.contains(ipAddress)) {
+                    uniqueIPsInRange.add(ipAddress);
+                }
+            }
+        }
+        return uniqueIPsInRange.size();
+    }
+    public static Date parseDate(String dateStr) {
+        ParsePosition pp = new ParsePosition(0);
+        return  dateFormat.parse(dateStr, pp);
+    }
+    public HashMap<String,Integer> countVisitsPerIP() {
+        HashMap<String,Integer> counts = new HashMap<String,Integer>();
+        
+        for (LogEntry le : records) {
+            String ip = le.getIpAddress();
+            if (!counts.containsKey(ip)) {
+                counts.put(ip, 1);
+            } else {
+                counts.put(ip, counts.get(ip) + 1);
+            }
+        }
+        return counts;
+    }
+    public int mostNumberVisitsByIP(HashMap<String, Integer> counts) {
+        int mostNumberVisits = 0;
+        for (String ip : counts.keySet() ) {
+            if (counts.get(ip) > mostNumberVisits) {
+                mostNumberVisits = counts.get(ip);
+            }
+        }
+        return mostNumberVisits;
+    }
+    public ArrayList<String> iPsMostVisits(HashMap<String, Integer> counts) {
+        ArrayList<String> ipWithMaxVisits = new ArrayList<String>();
+        int maxVisitNumber = mostNumberVisitsByIP(counts);
+        int currentIpVisitNumber;
+        for (String ip : counts.keySet()) {
+            currentIpVisitNumber = counts.get(ip);
+            if (currentIpVisitNumber == maxVisitNumber) {
+                ipWithMaxVisits.add(ip);
+            }
+        }
+        return ipWithMaxVisits;
+    }    
+    public HashMap<String, ArrayList<String>> iPsForDays() {
+        HashMap<String, ArrayList<String>> ipForDays = new HashMap<String, ArrayList<String>>();
+        for (LogEntry le : records) {
+            String date = le.getAccessTime().toString().substring(4,10);
+            String ip = le.getIpAddress();
+            if (!ipForDays.containsKey(date)){
+                ArrayList<String> ips = new ArrayList<String>();
+                ips.add(ip);
+                ipForDays.put(date, ips);
+            } else {
+                ipForDays.get(date).add(ip);
+            }
+            
+        }    
+        return ipForDays;
+    }
 }
--- WebServerLogsModule/Tester.java
+++ WebServerLogsModule/Tester.java
@@ -11,13 +11,43 @@ import java.util.*;
 public class Tester
 {
     public void testLogEntry() {
+        
         LogEntry le = new LogEntry("1.2.3.4", new Date(), "example request", 200, 500);
         System.out.println(le);
         LogEntry le2 = new LogEntry("1.2.100.4", new Date(), "example request 2", 300, 400);
         System.out.println(le2);
+        
     }
     
     public void testLogAnalyzer() {
-        // complete method
+        
+    	 LogAnalyzer la = new LogAnalyzer();
+    	 la.readFile("WebServerLogsModule/weblog3-short_log");
+    	 la.printAll();
+    	 int uniqueIPs = la.countUniqueIPs();
+    	 System.out.println("There are " + uniqueIPs + " unique IPs");
+    	 System.out.println("Unique visitors on Sep 14:");
+    	 System.out.println(la.uniqueIPVisitsOnDay("Sep 14").size());
+    	 System.out.println("Unique visitors on Sep 30:");    	 
+    	 System.out.println(la.uniqueIPVisitsOnDay("Sep 30").size());
+    	 System.out.println("Unique ips in range 200,299:");
+    	 System.out.println(la.countUniqueIPsInRange(200,299));
+    	 System.out.println("Unique ips in range 300,399:");
+    	 System.out.println(la.countUniqueIPsInRange(300,399));
+    	 System.out.println("printAllHigherThanNum using 400");
+    	 la.printAllHigherThanNum(400);
+    	 System.out.println("Unique visitors on Mar 24:");    	 
+    	 System.out.println(la.uniqueIPVisitsOnDay("Mar 24").size());
+    	 System.out.println("countVisitsPerIP:");
+    	 HashMap<String,Integer> counts = la.countVisitsPerIP();
+    	 System.out.println(counts);
+ 	 System.out.println("mostNumbervisitsByIP:");
+    	 System.out.println(la.mostNumberVisitsByIP(counts));
+ 	 System.out.println("iPsMostVisits:");
+    	 System.out.println(la.iPsMostVisits(counts));  
+ 	 System.out.println("iPsForDays:");
+    	 System.out.println(la.iPsForDays());  
+    	 
     }
+
 }
