2c709f872f624b3558067e675fdeffa5b3efc130
Davide Nastri <d.nastri@gmail.com>
2018-03-07 11:43:49 +0100

    feat: Added last check to Tester class

--- WebServerLogsModule/Tester.java
+++ WebServerLogsModule/Tester.java
@@ -21,39 +21,48 @@ public class Tester
     
     public void testLogAnalyzer() {
         
-    	 LogAnalyzer la = new LogAnalyzer();
-    	 la.readFile("WebServerLogsModule/weblog3-short_log");
-    	 la.printAll();
-    	 int uniqueIPs = la.countUniqueIPs();
-    	 System.out.println("There are " + uniqueIPs + " unique IPs");
-    	 System.out.println("Unique visitors on Sep 14:");
-    	 System.out.println(la.uniqueIPVisitsOnDay("Sep 14").size());
-    	 System.out.println("Unique visitors on Sep 30:");    	 
-    	 System.out.println(la.uniqueIPVisitsOnDay("Sep 30").size());
-    	 System.out.println("Unique ips in range 200,299:");
-    	 System.out.println(la.countUniqueIPsInRange(200,299));
-    	 System.out.println("Unique ips in range 300,399:");
-    	 System.out.println(la.countUniqueIPsInRange(300,399));
-    	 System.out.println("printAllHigherThanNum using 400");
-    	 la.printAllHigherThanNum(400);
-    	 System.out.println("Unique visitors on Mar 24:");    	 
-    	 System.out.println(la.uniqueIPVisitsOnDay("Mar 24").size());
-    	 System.out.println("countVisitsPerIP:");
-    	 HashMap<String,Integer> counts = la.countVisitsPerIP();
-    	 System.out.println(counts);
- 	 System.out.println("mostNumbervisitsByIP:");
-    	 System.out.println(la.mostNumberVisitsByIP(counts));
- 	 System.out.println("iPsMostVisits:");
-    	 System.out.println(la.iPsMostVisits(counts));  
- 	 System.out.println("iPsForDays:");
-    	 System.out.println(la.iPsForDays()); 
- 	 System.out.println("dayWithMostIPVisits:");
-    	 System.out.println(la.dayWithMostIPVisits(la.iPsForDays())); 
- 	 System.out.println("iPsWithMostVisitsOnDay:");
-         ArrayList<String> result = la.iPsWithMostVisitsOnDay(la.iPsForDays(), "Sep 30");
+         LogAnalyzer la = new LogAnalyzer();
+         la.readFile("WebServerLogsModule/weblog2_log");
+         la.printAll();
+         int uniqueIPs = la.countUniqueIPs();
+         System.out.println("There are " + uniqueIPs + " unique IPs");
+         System.out.println("Unique visitors on Sep 24:");
+         System.out.println(la.uniqueIPVisitsOnDay("Sep 24").size());
+         System.out.println("Unique visitors on Sep 30:");       
+         System.out.println(la.uniqueIPVisitsOnDay("Sep 30").size());
+         System.out.println("Unique ips in range 200,299:");
+         System.out.println(la.countUniqueIPsInRange(200,299));
+         System.out.println("Unique ips in range 400,499:");
+         System.out.println(la.countUniqueIPsInRange(400,499));
+         System.out.println("printAllHigherThanNum using 400");
+         la.printAllHigherThanNum(400);
+         System.out.println("Unique visitors on Mar 24:");       
+         System.out.println(la.uniqueIPVisitsOnDay("Mar 24").size());
+         System.out.println("countVisitsPerIP:");
+         HashMap<String,Integer> counts = la.countVisitsPerIP();
+         System.out.println(counts);
+     System.out.println("mostNumbervisitsByIP:");
+         System.out.println(la.mostNumberVisitsByIP(counts));
+     System.out.println("iPsMostVisits:");
+         System.out.println(la.iPsMostVisits(counts));  
+     System.out.println("iPsForDays:");
+         System.out.println(la.iPsForDays()); 
+     System.out.println("dayWithMostIPVisits:");
+         System.out.println(la.dayWithMostIPVisits(la.iPsForDays())); 
+     System.out.println("iPsWithMostVisitsOnDay:");
+         ArrayList<String> result = la.iPsWithMostVisitsOnDay(la.iPsForDays(), "Sep 29");
          for (int k = 0; k < result.size(); k++) {
              System.out.println(result.get(k));
          }
     }
-
+    public void quiz() {
+        LogAnalyzer la = new LogAnalyzer();
+        la.readFile("WebServerLogsModule/weblog1_log");
+        HashMap<String,Integer> counts = la.countVisitsPerIP();
+        System.out.println(counts);
+        System.out.println("mostNumbervisitsByIP:");
+        System.out.println(la.mostNumberVisitsByIP(counts));
+        System.out.println("iPsMostVisits:");
+        System.out.println(la.iPsMostVisits(counts));        
+    }
 }
